b7781c012f2b0f2f38e97640180fed875435e937
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-12 13:55:15 -0300

    Primeiro commit do projeto

---
+++ scripts.js
@@ -0,0 +1,40 @@
+console.log('SOMA DAS VARIÁVEIS ', num + num2)
+console.log('SUBTRAÇÃO DAS VARIÁVEL ', num3 - num)
+console.log('MULTIPLICAÇÃO ', num5 * 2)
+console.log('DIVISÃO DE VALORES ', num2 / 5)
+console.log('RESTO DA DIVISÃO ', num2 % 2)
+console.log('RESTO DA DIVISÃO ${(num3 % 3)}')
+
+// OPERADORES RELACIONAIS
+
+/*
+    == IGUAL
+    > MAIOR
+    < MENOR
+    >= MAIOR IGUAL
+    <= MENOR IGUAL
+    != DIFERENTE
+*/
+
+// OPERADORES LÓGICO
+
+/*
+    && E
+    || OU
+*/
+
+console.log('TESTE DE RELACIONAIS ', 10 == 5)
+console.log('TESTE RELACIONAIS ', 10 != 5)
+console.log('TESTE RELACIONAIS ', 10 > 5)
+console.log('TESTE RELACIONAIS ', 10 < 5)
+console.log('TESTE RELACIONAIS ', 10 >= 5)
+console.log('TESTE RELACIONAIS ', 10 <= 5)
+
+console.log('TESTE LÓGICO ', (10 > 5) && (15 < 8))
+console.log('TESTE LÓGICO ', (10 > 5) || (15 < 8))
+
+// TESTE LÓGICO
+
+let idade = 50
+
+if (idade >= 18)  {}
